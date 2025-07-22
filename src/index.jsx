4536ab29d28414c2e3f82278fbfcea518f9b4246
kikulik <nick.broadcaster@gmail.com>
2025-07-22 15:52:22 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AppWrapper from './App'; // Make sure this points to App.jsx
+
+console.log("index.jsx: Script is running"); // <-- ADD THIS
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppWrapper />); // Ensure you are rendering AppWrapper
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
