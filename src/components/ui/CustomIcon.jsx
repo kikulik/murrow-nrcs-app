973b3336808984aa6017a36592d0feefbdac29ae
kikulik <nick.broadcaster@gmail.com>
2025-07-23 13:22:27 +0300

    Create CustomIcon.jsx

---
+++ src/components/ui/CustomIcon.jsx
@@ -0,0 +1,27 @@
+// src/components/ui/CustomIcon.jsx
+// Custom icon component system for using uploaded PNG icons
+import React from 'react';
+
+const CustomIcon = ({ name, size = 20, className = '', alt = '' }) => {
+    const getIconPath = (iconName) => {
+        return `/src/assets/icons/${iconName}.png`;
+    };
+
+    const sizeInPixels = `${size}px`;
+
+    return (
+        <img
+            src={getIconPath(name)}
+            alt={alt || name}
+            className={className}
+            style={{
+                width: sizeInPixels,
+                height: sizeInPixels,
+                objectFit: 'contain',
+                display: 'inline-block'
+            }}
+        />
+    );
+};
+
+export default CustomIcon;
