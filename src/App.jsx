2fc934619c59af2a116591d45f4103ce0ae60929
kikulik <nick.broadcaster@gmail.com>
2025-07-23 16:51:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import { AppProvider } from './context/AppContext';
+import { CollaborationProvider } from './context/CollaborationContext';
 import AuthPage from './features/auth/AuthPage';
 import MurrowNRCS from './features/MurrowNRCS';
 import GlobalStyles from './components/ui/GlobalStyles';
@@ -11,10 +12,12 @@ import GlobalStyles from './components/ui/GlobalStyles';
 const AppWrapper = () => (
   <AuthProvider>
     <AppProvider>
-      <DndProvider backend={HTML5Backend}>
-        <GlobalStyles />
-        <App />
-      </DndProvider>
+      <CollaborationProvider>
+        <DndProvider backend={HTML5Backend}>
+          <GlobalStyles />
+          <App />
+        </DndProvider>
+      </CollaborationProvider>
     </AppProvider>
   </AuthProvider>
 );
