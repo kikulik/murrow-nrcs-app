bcb9c9e0b5c2be4cd947c609e4d0753b63f925d7
kikulik <nick.broadcaster@gmail.com>
2025-07-23 14:14:54 +0300

    Update AssignmentEditor.jsx

--- src/features/assignments/components/AssignmentEditor.jsx
+++ src/features/assignments/components/AssignmentEditor.jsx
@@ -70,7 +70,7 @@ const AssignmentEditor = ({ assignment, onSave, onCancel }) => {
                 <div className="flex items-center justify-end space-x-3 pt-4">
                     <button type="button" onClick={onCancel} className="btn-secondary">Cancel</button>
                     <button type="submit" className="btn-primary">
-                        <CustomIcon name="save" size={16} />
+                        <CustomIcon name="save" size={32} />
                         <span>Save</span>
                     </button>
                 </div>
