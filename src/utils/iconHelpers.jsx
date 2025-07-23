4619919590071646aba29ce4ac14d20659cca3f6
kikulik <nick.broadcaster@gmail.com>
2025-07-23 14:58:23 +0300

    Update iconHelpers.jsx

--- src/utils/iconHelpers.jsx
+++ src/utils/iconHelpers.jsx
@@ -3,9 +3,9 @@ import CustomIcon from '../components/ui/CustomIcon';
 
 export const getPlatformIcon = (platform) => {
     switch (platform) {
-        case 'broadcast': return <CustomIcon name="rundown" size={16} />;
-        case 'web': return <CustomIcon name="story" size={16} />;
-        case 'social': return <CustomIcon name="chat" size={16} />;
-        default: return <CustomIcon name="stories" size={16} />;
+        case 'broadcast': return <CustomIcon name="rundown" size={32} />;
+        case 'web': return <CustomIcon name="story" size={32} />;
+        case 'social': return <CustomIcon name="chat" size={32} />;
+        default: return <CustomIcon name="stories" size={32} />;
     }
 };
