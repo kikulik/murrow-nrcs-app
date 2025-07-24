9e0fe51db1d4ea86396074dff667bbb5d5e067d9
kikulik <nick.broadcaster@gmail.com>
2025-07-24 12:49:32 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,7 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {
-            gridTemplateColumns: {
-                '13': 'repeat(13, minmax(0, 1fr))',
-            },
-            gridColumn: {
-                'span-13': 'span 13 / span 13',
-            }
-        },
+        extend: {},
     },
     plugins: [],
 }
