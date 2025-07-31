59f8ac14a614ca32567fac3e731ed73767146dc7
kikulik <nick.broadcaster@gmail.com>
2025-07-31 11:19:01 +0300

    Update LiveModeTab.jsx

--- src/features/rundown/LiveModeTab.jsx
+++ src/features/rundown/LiveModeTab.jsx
@@ -43,7 +43,7 @@ const LiveModeTab = ({ liveMode }) => {
 
     const sendCasparCommand = async (command) => {
         try {
-            const response = await fetch('http://192.168.15.61:3001/api/caspar-command', {
+            const response = await fetch('https://f037893a1558.ngrok-free.app -> http://localhost:3001', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ command })
